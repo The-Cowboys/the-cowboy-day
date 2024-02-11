2842d646bc7089cf12242324b0866f49a7bb4a83
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-02-11 09:56:00 -0300

    Cambiando el Nav de ubicación.
    Estoy haciendo algunos cambios en el Nav. Decidí cambiar su ubicación porque planeo agregar más archivos relacionados a él. Ahora lo mantendré todo organizado en una carpeta exclusiva para todo lo relacionado con el NavBar.

--- frontend/src/Componentes/NavBar/NavBar.jsx
+++ frontend/src/Componentes/NavBar/NavBar.jsx
@@ -8,6 +8,7 @@ const NavBar = () => {
       <nav className="nav">
         <div>
           <h2 className="tituloNav">The Cowboys</h2>
+          <img></img>
           <ul className="elementosNav">
             <li className="boton"><Link to={"/"}>Inicio</Link></li>
             <li className="boton"><Link to={"/Estadisticas"}>Estadisticas</Link></li>
