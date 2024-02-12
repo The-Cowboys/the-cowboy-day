e9426830c12443cf6c757ec4e05e07e99a73bcc8
Franco Sangiacomo <f.sangiacomo@stuart.com>
2024-02-12 13:11:44 +0100

    Mueve Cowboy del Dia a un componente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,12 @@
-import { useState } from 'react';
 import './App.css'
 import NavBar from './Componentes/NavBar/NavBar';
 import Estructura from './Componentes/Estructura';
 
 function App() {
-  const [puto, setPuto] = useState(null);
-  const [clicked, setClick] = useState(false);
-
-  const click = () => {
-    setClick(true);
-
-    setTimeout(() => {
-      fetchPuto();
-    }, 1000);
-  };
-
-  const fetchPuto = async () => {
-    try {
-      const response = await fetch('/api/putos/today');
-      const data = await response.json();
-      setPuto(data.name);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
   return (
     <>
-    <NavBar />
+      <NavBar />
       <Estructura />
-      <div className="card">
-        {
-          !clicked ? (
-            <button onClick={click}>
-              ¿Quien es el Cowboy del día?
-            </button>
-          ) : (
-            <div>
-              <p>El cowboy de hoy es....</p>
-              <h2>{puto}</h2>
-            </div>
-          )
-        }
-      </div>
     </>
   )
 }
---
+++ frontend/src/Componentes/Pagina/CowboyDelDia.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const Inicio = () => {
+  const [puto, setPuto] = useState(null);
+  const [clicked, setClick] = useState(false);
+
+  const click = () => {
+    setClick(true);
+
+    setTimeout(() => {
+      fetchPuto();
+    }, 1000);
+  };
+
+  const fetchPuto = async () => {
+    try {
+      const response = await fetch('/api/putos/today');
+      const data = await response.json();
+      setPuto(data.name);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div className="card">
+        {
+          !clicked ? (
+            <button onClick={click}>
+              ¿Quien es el Cowboy del día?
+            </button>
+          ) : (
+            <div>
+              <p>El cowboy de hoy es....</p>
+              <h2>{puto}</h2>
+            </div>
+          )
+        }
+      </div>
+  );
+};
+
+export default Inicio;
