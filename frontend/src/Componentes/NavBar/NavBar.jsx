ed98968299f9ba2613a893bfa0b5e85da7a3aa96
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-02-15 19:06:00 -0300

    Lista de nombres.
    Completado el HTML y el CSS para la lista de nombres y logros. Todo está actualizado hasta hoy, 15/02/2024.
    
    También realicé modificaciones en los estilos CSS del archivo EstilosNavBar.css. En ese momento, utilicé clases de Bootstrap para cambiar los estilos, lo que causó conflictos con la lista de nombres, ya que ambas están usando Bootstrap y algunas etiquetas comparten las mismas clases. Sin embargo, he resuelto el problema cambiando las clases en el archivo EstilosNavBar.css y añadiendo las nuevas clases al archivo del NavBar.

--- frontend/src/Componentes/NavBar/NavBar.jsx
+++ frontend/src/Componentes/NavBar/NavBar.jsx
@@ -5,13 +5,13 @@ const NavBar = () => {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="nav container-fluid">
-          <div className="d-flex align-items-center">
+          <div className="tituloCont d-flex align-items-center">
             <Link className="tituloNav navbar-brand" to="/">
               The Cowboys
             </Link>
           </div>
           <button
-            className="navbar-toggler"
+            className="botonDelNav navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavDropdown"
@@ -24,7 +24,7 @@ const NavBar = () => {
               width="32"
               height="32"
               fill="currentColor"
-              className="bi bi-crosshair"
+              className="fotoimg bi bi-crosshair"
               viewBox="0 0 16 16"
             >
               <path d="M8.5.5a.5.5 0 0 0-1 0v.518A7 7 0 0 0 1.018 7.5H.5a.5.5 0 0 0 0 1h.518A7 7 0 0 0 7.5 14.982v.518a.5.5 0 0 0 1 0v-.518A7 7 0 0 0 14.982 8.5h.518a.5.5 0 0 0 0-1h-.518A7 7 0 0 0 8.5 1.018zm-6.48 7A6 6 0 0 1 7.5 2.02v.48a.5.5 0 0 0 1 0v-.48a6 6 0 0 1 5.48 5.48h-.48a.5.5 0 0 0 0 1h.48a6 6 0 0 1-5.48 5.48v-.48a.5.5 0 0 0-1 0v.48A6 6 0 0 1 2.02 8.5h.48a.5.5 0 0 0 0-1zM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
@@ -60,22 +60,22 @@ const NavBar = () => {
                 >
                   Estadisticas
                 </Link>
-                <ul className="dropdown-menu">
+                <ul className="menuEsta dropdown-menu">
                   <li>
-                    <Link className="dropdown-item" to="EstadisticasElMasToto">
+                    <Link className="menuEstadisticas dropdown-item" to="EstadisticasElMasToto">
                       El más toto
                     </Link>
                   </li>
                   <li>
                     <Link
-                      className="dropdown-item"
+                      className="menuEstadisticas dropdown-item"
                       to="EstadisticasCowboyDelDia"
                     >
                       Cowboy Del Dia
                     </Link>
                   </li>
                   <li>
-                    <Link className="dropdown-item" to="Proximamente">
+                    <Link className="menuEstadisticas dropdown-item" to="Proximamente">
                       --Próximamente--
                     </Link>
                   </li>
