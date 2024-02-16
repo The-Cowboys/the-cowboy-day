53f2b1115d02870f528cc29969a90eab943879c3
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-02-16 11:11:17 -0300

    PantallaDeListadeCowboy (#8)
    * Archivos
    
    Estoy preparando los archivos para comenzar a trabajar en ellos.
    
    * Lista de nombres.
    
    Completado el HTML y el CSS para la lista de nombres y logros. Todo está actualizado hasta hoy, 15/02/2024.
    
    También realicé modificaciones en los estilos CSS del archivo EstilosNavBar.css. En ese momento, utilicé clases de Bootstrap para cambiar los estilos, lo que causó conflictos con la lista de nombres, ya que ambas están usando Bootstrap y algunas etiquetas comparten las mismas clases. Sin embargo, he resuelto el problema cambiando las clases en el archivo EstilosNavBar.css y añadiendo las nuevas clases al archivo del NavBar.
    
    * Un pequeño añadido
    
    Estoy considerando implementar una función que ordene los nombres de mayor a menor según la frecuencia con la que aparecieron en el más tonto.
    
    * Cambio de nombre

--- frontend/src/Componentes/Estructura.jsx
+++ frontend/src/Componentes/Estructura.jsx
@@ -3,6 +3,9 @@ import CowboyDelDia from "./Pagina/CowboyDelDia";
 import Estadisticas from "./Pagina/Estadisticas";
 import Acerca from "./Pagina/Acerca";
 import Contacto from "./Pagina/Contacto";
+import ListaElMasToto from "./Pagina/Estadisticas/ListaElMasToto";
+import EstadisticasCowboyDelDia from "./Pagina/Estadisticas/EstadisticasCowboyDelDia";
+import Proximamente from "./Pagina/Estadisticas/Proximamente";
 
 const Estructura = () => {
   return (
@@ -13,6 +16,9 @@ const Estructura = () => {
           <Route path="/Estadisticas" element={<Estadisticas />} />
           <Route path="/Acerca" element={<Acerca />} />
           <Route path="/Contacto" element={<Contacto />} />
+          <Route path="/ListaElMasToto" element={<ListaElMasToto />} />
+          <Route path="/EstadisticasCowboyDelDia" element={<EstadisticasCowboyDelDia />} />
+          <Route path="/Proximamente" element={<Proximamente />} />
         </Routes>
       </div>
     </>
---
+++ frontend/src/Componentes/Pagina/Estadisticas/EstadisticasCowboyDelDia.jsx
@@ -0,0 +1,11 @@
+const EstadisticasCowboyDelDia = () => {
+  return (
+    <>
+      <section>
+        <h3>--Estadisticas del cowboy del día en mantenimiento--</h3>
+      </section>
+    </>
+  );
+};
+
+export default EstadisticasCowboyDelDia;
