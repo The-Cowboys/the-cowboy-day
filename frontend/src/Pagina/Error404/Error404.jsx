e39fc9d5fbe99939cc11aaa6722a114068eef458
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-02-28 07:19:07 -0300

    Organización de código

--- frontend/src/Pagina/Error404/Error404.jsx
+++ frontend/src/Pagina/Error404/Error404.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Botones from "../../Botones.jsx";
+import Botones from "../../Componentes/Botones.jsx";
 import "./Error404.css";
 
 
--- frontend/src/Pagina/InfoTonto/ListaParticipantes.jsx
+++ frontend/src/Pagina/InfoTonto/ListaParticipantes.jsx
@@ -1,5 +1,5 @@
 import ListaTitulos from "./ListaTitulos";
-import { useNombres } from "../../Api/Api.JS";
+import { useNombres } from "../../../API/Api.JS";
 
 const ListaParticipantes = () => {
   const { users } = useNombres();
