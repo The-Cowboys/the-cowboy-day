db79467ed5868b7325d8f02cac1b8eeb5af4b793
Franco Sangiacomo <franconecat@gmail.com>
2024-03-01 11:43:38 +0100

    Add comments

--- server/api.js
+++ server/api.js
@@ -9,10 +9,13 @@ const putosRoute = require('./putos/putos.route');
 router.use('/cowboys', cowboysRoute);
 router.use('/putos', putosRoute); // TODO: remove
 
+// Solicitud `GET /cowboys/idCowboy/titulos` para obtener los títulos de un cowboy por id
 router.get( '/cowboys/:idCowboy/titulos', titulosController.getCowboyTitulos);
+// Solicitud `POST /cowboys/idCowboy/titulos` para crear un titulo al un cowboy por id
 router.post('/cowboys/:idCowboy/titulos', titulosController.createCowboyTitulo);
-
+// Solicitud `GET /titulos para obtener` todos los títulos
 router.get('/titulos', titulosController.getAllTitulos);
+// Solicitud `DELETE /titulos/:idTitulo` para obtener todos los títulos
 router.delete('/titulos/:idTitulo', titulosController.deleteTitulo);
 
 module.exports = router;
