d24f39580c9675ee1a65916e603b9235af3a337a
Franco Sangiacomo <franconecat@gmail.com>
2024-03-07 09:36:00 +0100

    Rename toModel

--- server/titulos/titulos.repository.js
+++ server/titulos/titulos.repository.js
@@ -1,6 +1,6 @@
 const pool = require("../database");
 
-function toModel(row) {
+function convertRowToObject(row) {
   return {
     id: row.id,
     titulo: row.name,
@@ -11,7 +11,7 @@ function toModel(row) {
 
 async function getAll() {
   const results = await pool.query("SELECT * FROM titulos ORDER BY created");
-  return results.rows.map(toModel);
+  return results.rows.map(convertRowToObject);
 }
 
 async function deleteById(id) {
@@ -24,7 +24,7 @@ async function getAllByCowboyId(id) {
     [id]
   );
 
-  return results.rows.map(toModel);
+  return results.rows.map(convertRowToObject);
 }
 
 async function saveTitulo(cowboyId, data) {
@@ -36,7 +36,7 @@ async function saveTitulo(cowboyId, data) {
     RETURNING *`,
     [data.name, cowboyId, data.created]
   );
-  return toModel(result.rows[0]);
+  return convertRowToObject(result.rows[0]);
 }
 
 module.exports = {
