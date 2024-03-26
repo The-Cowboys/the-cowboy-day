5cb22aa60a83510960a7b434a180c2f1f337fd4a
Franco Sangiacomo <franconecat@gmail.com>
2024-03-26 13:10:18 +0100

    Simplificar las respuestas de la API

--- server/tontos/tontos.repository.js
+++ server/tontos/tontos.repository.js
@@ -1,14 +1,31 @@
 const pool = require("./../database");
 
 async function getTontoByDate(dayStr) {
-  const res = await pool.query("SELECT * FROM tontos WHERE dia = $1", [dayStr]);
+  const res = await pool.query(
+    `
+    SELECT 
+      t.dia dia, 
+      c.id AS id,
+      c.name AS name,
+      ti.name AS titulo,
+      COUNT(t.cowboy_id) AS total
+    FROM tontos t
+    INNER JOIN cowboys c ON t.cowboy_id = c.id
+    LEFT JOIN titulos ti ON ti.cowboy_id = c.id
+    WHERE t.dia = $1
+    GROUP BY t.dia, c.id, c.name, ti.name;
+    `,
+    [dayStr]
+  );
+
   if (res.rows.length > 0) {
-    const data = res.rows[0];
+    const firstRow = res.rows[0];
     return {
-      id: data.id,
-      dia: data.dia,
-      cowboyId: data.cowboy_id,
-      created: data.created,
+      dia: firstRow.dia,
+      id: firstRow.id,
+      nombre: firstRow.name,
+      total: firstRow.total,
+      titulos: res.rows.map((row) => row.titulo),
     };
   }
   return null;
@@ -35,19 +52,37 @@ async function saveTonto(dia, cowboyId, created) {
 
 async function getTontos() {
   const res = await pool.query(
-    `SELECT 
-      cowboys.id, 
-      cowboys.name, 
-      (SELECT count(tontos.cowboy_id) FROM tontos WHERE tontos.cowboy_id = cowboys.id) as total
-    FROM cowboys`
+    `
+    SELECT 
+      c.id AS id,
+      c.name AS name,
+      ti.name AS titulo,
+      COUNT(t.cowboy_id) AS total
+    FROM cowboys c
+    INNER JOIN tontos t ON t.cowboy_id = c.id
+    LEFT JOIN titulos ti ON ti.cowboy_id = c.id
+    GROUP BY c.id, c.name, ti.name;
+    `
   );
-  return res.rows.map((row) => {
-    return {
-      id: row.id,
-      name: row.name,
-      total: row.total,
-    };
-  });
+
+  const groupedData = res.rows.reduce((acc, row) => {
+    if (acc.has(row.id)) {
+      const data = acc.get(row.id);
+      data.titulos.push(row.titulo);
+    } else {
+      const titulos = [];
+      if (row.titulo) titulos.push(row.titulo);
+      acc.set(row.id, {
+        id: row.id,
+        nombre: row.name,
+        total: row.total,
+        titulos: titulos,
+      });
+    }
+    return acc;
+  }, new Map());
+
+  return Array.from(groupedData.values());
 }
 
 module.exports = {
