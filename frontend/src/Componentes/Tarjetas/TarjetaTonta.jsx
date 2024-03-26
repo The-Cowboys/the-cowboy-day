334d421f877295de11b1e0b53ec46c6528254b71
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-03-26 16:35:22 -0300

    TarjetaTonta Pronta

--- frontend/src/Componentes/Tarjetas/TarjetaTonta.jsx
+++ frontend/src/Componentes/Tarjetas/TarjetaTonta.jsx
@@ -3,35 +3,40 @@ import { useObtenerTonto } from "../../Hooks/useObtenerTonto";
 const TarjetaTonta = () => {
   const { tonto } = useObtenerTonto();
 
-  return (
-    <div className="principalTarjeta">
-      <div className="card mb-3">
-        <div className="row g-0">
-          <div className="fondoNav col-md-4">
-            <img
-              src="/Cowboys.png"
-              className="fondoNav img-fluid rounded-start"
-              alt="Icono cowboys"
-            />
-          </div>
-          <div className="fondoNav col-md-8">
-            <div className="fondoNav card-body">
-              <h5 className="nombre card-title">
-                Nombre: {tonto.nombre}
-                <span className="numeros badge float-right">{tonto.total}</span>
-              </h5>
-              <h6 className="fondoNav card-text">Titulos totales: 4</h6>
-              <div>
-                <p className="titulosObtenidos fondoNav">
-                  Titulos: {tonto.titulos.join(" - ")}
-                </p>
+  if (tonto != "")
+    return (
+      <div className="principalTarjeta">
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="fondoNav col-md-4">
+              <img
+                src="/Cowboys.png"
+                className="fondoNav img-fluid rounded-start"
+                alt="Icono cowboys"
+              />
+            </div>
+            <div className="fondoNav col-md-8">
+              <div className="fondoNav card-body">
+                <h5 className="nombre card-title">
+                  Nombre: {tonto.nombre}
+                  <span className="numeros badge float-right">
+                    {tonto.total}
+                  </span>
+                </h5>
+                <h6 className="fondoNav card-text">
+                  Titulos totales: {tonto.titulos.length}
+                </h6>
+                <div>
+                  <p className="titulosObtenidos fondoNav">
+                    Titulos: {tonto.titulos.join(" - ")}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default TarjetaTonta;
