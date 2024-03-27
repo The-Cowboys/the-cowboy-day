55e38724fe553a900406d1c0d4e6de33f1cb05af
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-03-27 15:34:15 -0300

    Incluir id de cowboy en la ruta de Titulos (#43)
    * Incluir id de cowboy en la ruta de Titulos
    
    * Manejo de errores

---
+++ frontend/src/Hooks/useObtenerParticipante.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { obtenerTontoId } from "../API/Api";
+import { useNavigate } from "react-router-dom";
+
+export const useObtenerParticipante = (idCowboy) => {
+  const [participante, setParticipante] = useState([]);
+  const navigate = useNavigate();
+
+  const mostrarParticipante = async () => {
+    try {
+      const data = await obtenerTontoId(idCowboy);
+      setParticipante(data);
+    } catch (err) {
+      navigate("/*");
+    }
+  };
+  useEffect(() => {
+    mostrarParticipante();
+  }, []);
+
+  return {
+    participante,
+  };
+};
--- frontend/src/Pagina/PerfilDeParticipante/ListaParticipantes.jsx
+++ frontend/src/Pagina/PerfilDeParticipante/ListaParticipantes.jsx
@@ -1,4 +1,4 @@
-import { useNombres } from "../../Hooks/obtenerNombres"
+import { useNombres } from "../../Hooks/obtenerNombres";
 import { Link } from "react-router-dom";
 
 const ListaParticipantes = () => {
@@ -9,9 +9,13 @@ const ListaParticipantes = () => {
         <ol className="list-group list-group-numbered" key={index}>
           <li className="list-group-item d-flex justify-content-between align-items-start">
             <div className="logros ms-2 me-auto">
-              <Link to="/EstructuraDePerfil" state={{ user: user }} className="nombre fw-bold">{user.name}</Link>
-              <div className="fondoNav">
-              </div>
+              <Link
+                to={`/EstructuraDePerfil/${user.id}`}
+                className="nombre fw-bold"
+              >
+                {user.name}
+              </Link>
+              <div className="fondoNav"></div>
             </div>
             <span className="badge">75</span>
           </li>
