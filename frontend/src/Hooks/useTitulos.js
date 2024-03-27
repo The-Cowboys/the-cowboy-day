83466267d1de73751eff7f2a24d6fec2f50ef960
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-03-27 16:22:42 -0300

    Pronto (#45)
    * Pronto
    
    Tengo un pero.
    Siento que resolví un problema por instinto o casualidad, pero al mismo tiempo sé que va ahí.
    Te cuento más tarde! 😉🔍
    
    * Update ListaTitulos.jsx
    
    * Borrando comentarios

---
+++ frontend/src/Hooks/useTitulos.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { deleteTitulos, getTitulos, postTitulos } from "../API/Api";
+
+const useTitulos = (idCowboy) => {
+  const [input, setInput] = useState("");
+  const [titulos, setTitulos] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    mostrarTitulos();
+  }, []);
+
+  const mostrarTitulos = async () => {
+    const lista = await getTitulos(idCowboy);
+    setTitulos(lista);
+  };
+
+  const crearTitulo = async () => {
+    const titulo = {
+      titulo: input,
+    };
+    try {
+      const tituloCreado = await postTitulos(idCowboy, titulo);
+      const nuevaLista = [...titulos, tituloCreado];
+      setTitulos(nuevaLista);
+      setError(false);
+    } catch (err) {
+      setError(true);
+      console.log("Hubo un error!!!!!");
+    }
+    setInput("");
+  };
+
+  const borrarTitulo = async (id) => {
+    await deleteTitulos(id);
+    const listaBorrada = titulos.filter((titulo) => titulo.id != id);
+    setTitulos(listaBorrada);
+  };
+
+  return {
+    borrarTitulo,
+    crearTitulo,
+    error,
+    titulos,
+    setInput,
+    input,
+  };
+};
+
+export default useTitulos;
