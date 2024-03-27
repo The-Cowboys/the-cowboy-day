e632dc5969f994600cf01a626f1dc00816cbc15f
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-03-27 14:44:56 -0300

    Borrando comentarios

--- frontend/src/Hooks/useTitulos.js
+++ frontend/src/Hooks/useTitulos.js
@@ -2,25 +2,20 @@ import { useEffect, useState } from "react";
 import { deleteTitulos, getTitulos, postTitulos } from "../API/Api";
 
 const useTitulos = (idCowboy) => {
-  // Estados
   const [input, setInput] = useState("");
   const [titulos, setTitulos] = useState([]);
   const [error, setError] = useState(false);
 
-  // Hooks
   useEffect(() => {
     mostrarTitulos();
   }, []);
 
-  // Acciones
   const mostrarTitulos = async () => {
-    // Llamar a la API para obtener los títulos
     const lista = await getTitulos(idCowboy);
     setTitulos(lista);
   };
 
   const crearTitulo = async () => {
-    // Llamar a la API para crear un nuevo título
     const titulo = {
       titulo: input,
     };
@@ -37,7 +32,6 @@ const useTitulos = (idCowboy) => {
   };
 
   const borrarTitulo = async (id) => {
-    // Llamar a la API para borrar un título
     await deleteTitulos(id);
     const listaBorrada = titulos.filter((titulo) => titulo.id != id);
     setTitulos(listaBorrada);
