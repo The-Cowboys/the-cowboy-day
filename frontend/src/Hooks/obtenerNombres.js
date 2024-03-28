2a7b4fb25cc64cf48aab3175a30e32b09c0fd1f1
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-03-27 23:10:45 -0300

    Pronto
    Con más lio en mi cabeza

--- frontend/src/Hooks/obtenerNombres.js
+++ frontend/src/Hooks/obtenerNombres.js
@@ -3,10 +3,12 @@ import { obtenerNombres } from "../API/Api";
 
 export const useNombres = () => {
   const [users, setUsers] = useState([]);
+  const [carga, setCarga] = useState(true);
 
   const mostrarUsuarios = async () => {
     const data = await obtenerNombres();
     setUsers(data);
+    setCarga(false);
   };
 
   useEffect(() => {
@@ -15,5 +17,6 @@ export const useNombres = () => {
 
   return {
     users,
+    carga,
   };
 };
