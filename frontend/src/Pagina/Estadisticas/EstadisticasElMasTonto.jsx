4042a92f25bb5090295a7120db79e1fcebf02411
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-03-29 13:34:50 -0300

    <Carga /> Mantenimiento.
    <Carga /> en los componentes que están en mantenimiento. 💻⚙️

--- frontend/src/Pagina/Estadisticas/EstadisticasElMasTonto.jsx
+++ frontend/src/Pagina/Estadisticas/EstadisticasElMasTonto.jsx
@@ -1,9 +1,12 @@
+import Carga from "../Errores/Carga";
+
 const EstadisticasElMasTonto = () => {
   return (
     <>
       <section>
         <h3>--Estadisticas el mas toto en mantenimiento--</h3>
       </section>
+      <Carga />
     </>
   );
 };
--- frontend/src/Pagina/Estadisticas/Proximamente.jsx
+++ frontend/src/Pagina/Estadisticas/Proximamente.jsx
@@ -1,9 +1,12 @@
+import Carga from "../Errores/Carga";
+
 const Proximamente = () => {
   return (
     <>
       <section>
         <h3>--Estadisticas Próximamente--</h3>
       </section>
+      <Carga />
     </>
   );
 };
