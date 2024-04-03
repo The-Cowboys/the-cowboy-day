c66d8b6a53797624d0a466650b6e5e1f82c48c66
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-04-03 18:04:43 -0300

    Cowboy Del Día
    El componente <CowboyDelDia /> cada vez se parece más al componente <ElMasTonto />. 🤠

--- frontend/src/Hooks/useCowboyDelDia.js
+++ frontend/src/Hooks/useCowboyDelDia.js
@@ -1,27 +1,23 @@
 // Cowboy Del Día
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchCowboysToday } from "../API/Api";
 
 export const useCowboyDelDia = () => {
   const [cowboy, setCowboy] = useState(null);
-  const [clicked, setClick] = useState(false);
-
-  const click = () => {
-    setClick(true);
-
-    setTimeout(() => {
-      fetchCowboys();
-    }, 1000);
-  };
+  const [clicked, setClick] = useState(true);
 
   const fetchCowboys = async () => {
     const cowboyName = await fetchCowboysToday();
     setCowboy(cowboyName);
+    setClick(false);
   };
+
+  useEffect(() => {
+    fetchCowboys();
+  }, []);
 
   return {
     cowboy,
     clicked,
-    click,
   };
 };
