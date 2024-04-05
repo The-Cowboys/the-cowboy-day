8d68f2cd49246e5278862b9613ee062d3cc7ad50
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-04-05 07:34:37 -0300

    Borrarndo todo lo relacionado con los clicks

--- frontend/src/Hooks/useCowboyDelDia.js
+++ frontend/src/Hooks/useCowboyDelDia.js
@@ -4,12 +4,10 @@ import { fetchCowboysToday } from "../API/Api";
 
 export const useCowboyDelDia = () => {
   const [cowboy, setCowboy] = useState(null);
-  const [clicked, setClick] = useState(true);
 
   const fetchCowboys = async () => {
     const cowboyName = await fetchCowboysToday();
     setCowboy(cowboyName);
-    setClick(false);
   };
 
   useEffect(() => {
@@ -18,6 +16,5 @@ export const useCowboyDelDia = () => {
 
   return {
     cowboy,
-    clicked,
   };
 };
