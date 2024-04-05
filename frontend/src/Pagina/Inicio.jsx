f4b739737e9b47cec73704d658cb8627ca36a746
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-04-05 15:04:24 -0300

    Mensaje cuando no hay tonto (#60)
    * Tarjeta cuando haya un error
    
    Un nuevo componente que va a mostrar una tarjeta cuando hay un error con el más tonto.
    Esto puede cambiar en el futuro a otra cosa.
    
    * Decidí borrar el componente de UnError
    
    Decidí borrar el componente de UnError.
    Me parece mejor usar la misma tarjeta. Esto, desde luego, puede cambiar. 🗑️🃏
    
    * Tarjeta de error del cowboy del día

--- frontend/src/Pagina/Inicio.jsx
+++ frontend/src/Pagina/Inicio.jsx
@@ -3,22 +3,31 @@ import Tarjeta from "../Componentes/Tarjeta/Tarjeta";
 import Carga from "../Pagina/Errores/Carga";
 
 const Inicio = () => {
-  const { tonto, carga } = useObtenerTonto();
+  const { tonto, carga, error } = useObtenerTonto();
 
   if (carga) {
     return <Carga />;
   }
-
   return (
     <>
       <section>
         {/* Tarjeta del día del más tonto */}
         <h3 className="tituloH3">Tonto del día de hoy es...</h3>
-        <Tarjeta
-          nombre={tonto.nombre}
-          total={tonto.total}
-          titulos={tonto.titulos}
-        />
+        {tonto && (
+          <Tarjeta
+            nombre={tonto.nombre}
+            total={tonto.total}
+            titulos={tonto.titulos}
+          />
+        )}
+        {error && (
+          // Tarjeta de error del día del más tonto
+          <Tarjeta
+            nombre={"Procesando un tonto"}
+            total={"--"}
+            titulos={["----"]}
+          />
+        )}
 
         {/* Tarjeta del cowboy del día */}
         <h3 className="tituloH3">Cowboy del día de hoy es...</h3>
@@ -27,6 +36,14 @@ const Inicio = () => {
           total={"--"}
           titulos={["--Próximamente--"]}
         />
+        {error && (
+          // Tarjeta de error del cowboy del día
+          <Tarjeta
+            nombre={"Procesando un cowboy"}
+            total={"--"}
+            titulos={["----"]}
+          />
+        )}
       </section>
     </>
   );
