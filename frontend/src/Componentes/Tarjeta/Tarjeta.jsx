21544bad2e3bf295668ba487ae607632da2d784c
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-04-06 09:36:56 -0300

    Tamaño de palabras en la tarjeta
    Ahora las tarjetas muestran la palabra 'Nombre', títulos totales y títulos en negrita.
    Además, son un poco más grandes.  🃏

--- frontend/src/Componentes/Tarjeta/Tarjeta.jsx
+++ frontend/src/Componentes/Tarjeta/Tarjeta.jsx
@@ -13,15 +13,17 @@ const TarjetaTonta = ({ nombre, total, titulos }) => {
           <div className="fondoNav col-md-8">
             <div className="fondoNav card-body">
               <h5 className="nombre card-title">
-                Nombre: {nombre}
+                <strong className="negrita">Nombre:</strong> {nombre}
                 <span className="numeros badge float-right">{total}</span>
               </h5>
               <h5 className="fondoNav card-text">
-                Titulos totales: {titulos.length}
+                <strong className="negrita">Titulos totales:</strong>{" "}
+                {titulos.length}
               </h5>
               <div>
                 <p className="titulosObtenidos fondoNav">
-                  Titulos: {titulos.join(", ")}
+                  <strong className="negrita">Titulos:</strong>{" "}
+                  {titulos.join(", ")}
                 </p>
               </div>
             </div>
