f329acc6fa86d404eb96a9eccc3ca8943cf26021
Franco Sangiacomo <franconecat@gmail.com>
2024-04-07 21:43:12 +0200

    Remove comment

--- sql/006_add_tonto_column.sql
+++ sql/006_add_tonto_column.sql
@@ -7,5 +7,4 @@ UPDATE cowboys SET tonto = 0 where id = 6;
 UPDATE cowboys SET tonto = 0 where id = 2;
 UPDATE cowboys SET tonto = 0 where id = 1;
 
--- make email not null
 ALTER TABLE cowboys ALTER COLUMN tonto SET NOT NULL;
