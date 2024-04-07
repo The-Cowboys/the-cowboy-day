e1884447f8bf57fac17e53c92f746e3e9795d112
Franco Sangiacomo <franconecat@gmail.com>
2024-04-08 00:05:10 +0200

    Arreglar autenticacion de titulos

--- server/api.js
+++ server/api.js
@@ -15,12 +15,12 @@ router.use("/putos", putosRoute); // TODO: remove
 // Solicitud `GET /cowboys/idCowboy/titulos` para obtener los títulos de un cowboy por id
 router.get("/cowboys/:idCowboy/titulos", titulosController.getCowboyTitulos);
 // Solicitud `POST /cowboys/idCowboy/titulos` para crear un titulo al un cowboy por id
-router.post("/cowboys/:idCowboy/titulos", titulosController.createCowboyTitulo);
+router.post("/cowboys/:idCowboy/titulos", standardAuth, titulosController.createCowboyTitulo);
 
 // Solicitud `GET /titulos para obtener` todos los títulos
 router.get("/titulos", titulosController.getAllTitulos);
 // Solicitud `DELETE /titulos/:idTitulo` para obtener todos los títulos
-router.delete("/titulos/:idTitulo", titulosController.deleteTitulo);
+router.delete("/titulos/:idTitulo", standardAuth, titulosController.deleteTitulo);
 
 // == Tontos ==
 // Guardar tonto del dia
