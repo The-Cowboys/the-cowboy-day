385f75c44575c68ccc261f886e841ac75b05fb14
Franco Sangiacomo <franconecat@gmail.com>
2024-04-07 22:59:09 +0200

    Borrar restriccion de unicidad de los titulos

---
+++ sql/007_update_tontos.sql
@@ -0,0 +1,6 @@
+UPDATE cowboys SET tonto = 89 where id = 4; -- gkroger11
+UPDATE cowboys SET tonto = 86 where id = 3; -- el_mallic
+UPDATE cowboys SET tonto = 81 where id = 5; -- dgraficohunter235
+UPDATE cowboys SET tonto = 83 where id = 6; -- cristianescobar47468
+UPDATE cowboys SET tonto = 86 where id = 2; -- franconecat
+UPDATE cowboys SET tonto = 77 where id = 1; -- yoomat.75
