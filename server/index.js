9593481d40d02ab6f15d6952c330f21244dd06fa
Franco Sangiacomo <franconecat@gmail.com>
2024-04-08 10:49:32 +0200

    Limitador de carga solo para la API, la web no va a estar protegida

--- server/index.js
+++ server/index.js
@@ -13,10 +13,9 @@ const PORT = 3000;
 
 const web = express.static("frontend/dist");
 
-// Set up rate limiter: maximum of twenty requests per minute
 const limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minute
-  max: 100,
+  windowMs: 1 * 60 * 1000,
+  max: 2,
 });
 
 
@@ -28,19 +27,20 @@ const hlmetHeaders = helmet.contentSecurityPolicy({
 
 // gzip compression
 app.use(compression());
+
 // set security HTTP headers
 app.use(hlmetHeaders);
-//  apply to all requests
-app.use(limiter);
+
 // enable cors
 app.use(cors());
+
 // parse json request body
 app.use(express.json());
+
 // static files
 app.use(express.static("frontend/dist"));
 
-app.use("/", web);
-app.use("/api", apiRoute);
+app.use("/api", limiter, apiRoute);
 app.use("*", web);
 
 // Start the server
