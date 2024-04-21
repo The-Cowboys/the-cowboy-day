1e35f735625e66bcdb9029d5e24410b80e007624
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-04-21 12:16:44 -0300

    Prueba Context (#73)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import NavBar from "./Componentes/NavBar/NavBar";
 import Estructura from "./Componentes/Estructura";
+import { PruebaContext } from "./Context/Context";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Estructura />
+      <PruebaContext.Provider value="yoooo">
+        <NavBar />
+        <Estructura />
+      </PruebaContext.Provider>
     </>
   );
 }
