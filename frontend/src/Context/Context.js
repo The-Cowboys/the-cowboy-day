b726ca8ae586712d183b0a7cbce283c919a3df06
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-04-21 18:31:36 -0300

    Estado Global
    Franco, esto medio que ya quedó, pero estoy teniendo unos problemas. La página no se está renderizando correctamente.

--- frontend/src/Context/Context.js
+++ frontend/src/Context/Context.js
@@ -1,3 +1,5 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
-export const PruebaContext = createContext(null);
+export const TContext = createContext();
+
+export const useEstadoGlobal = () => useContext(TContext);
