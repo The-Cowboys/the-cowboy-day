5c2fcae1a56996c646417b05fa4b83299bb263f6
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-05-02 19:49:54 -0300

    Calendario ubicación - 2
    De momento, va a estar acá. Luego veremos dónde dejarlo, o quizás agregamos un botón por ahí para ir directo al calendario. 🗓️

--- frontend/src/Pagina/ElMasTonto/ElMasTonto.jsx
+++ frontend/src/Pagina/ElMasTonto/ElMasTonto.jsx
@@ -2,7 +2,6 @@ import { useObtenerTonto } from "../../Hooks/useObtenerTonto";
 import ListaTontos from "./PerfilDeParticipante/ListaTontos";
 import Carga from "../Errores/Carga";
 import Tarjeta from "../../Componentes/Tarjeta/Tarjeta";
-import Calendario from "../../Componentes/Calendario";
 
 const ListaElMasTonto = () => {
   const { tonto, carga, errorTonto } = useObtenerTonto();
@@ -37,9 +36,6 @@ const ListaElMasTonto = () => {
         {/* Lista de participantes tontos */}
         <h3 className="tituloH3">Lista de participantes</h3>
         <ListaTontos />
-      </section>
-      <section className="calendarioTamaño">
-        <Calendario />
       </section>
     </>
   );
