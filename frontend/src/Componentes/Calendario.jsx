d1e95234f1589a528f48eaed7cabb9f46fbf7f13
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-05-04 20:42:05 -0300

    Estilos del calendario listos
    Incorporados estilos CSS al calendario y añadidas 3 pruebas de eventos.
    Todo listo para la acción  📆

--- frontend/src/Componentes/Calendario.jsx
+++ frontend/src/Componentes/Calendario.jsx
@@ -1,11 +1,45 @@
-import { Calendar } from "react-big-calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import dayjs from "dayjs";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "dayjs/locale/es";
+
+dayjs.locale("es");
 
 const Calendario = () => {
+  const localizar = dayjsLocalizer(dayjs);
+  const messages = {
+    today: "Hoy",
+    previous: "Mes anterior",
+    next: "Mes siguiente",
+  };
+
+  const events = [
+    {
+      start: dayjs("2024-05-09").toDate(),
+      end: dayjs("2024-05-11").toDate(),
+      title: "Prueba 1",
+    },
+    {
+      start: dayjs("2024-05-22").toDate(),
+      end: dayjs("2024-05-22").toDate(),
+      title: "Prueba 2",
+    },
+    {
+      start: dayjs("2024-06-22").toDate(),
+      end: dayjs("2024-06-22").toDate(),
+      title: "Prueba 3",
+    },
+  ];
+
   return (
     <>
       <div className="calendarioTamaño">
-        <Calendar />
+        <Calendar
+          localizer={localizar}
+          messages={messages}
+          views={["month"]}
+          events={events}
+        />
       </div>
     </>
   );
