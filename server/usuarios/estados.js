e3a6d3956ee834a1314fd576851ffeea04960a9a
Franco Sangiacomo <franconecat@gmail.com>
2025-10-18 12:05:06 +0200

    Add token generation utility

--- server/usuarios/estados.js
+++ server/usuarios/estados.js
@@ -1,6 +1,8 @@
 // Estados de usuario
 const ESTADO_ACTIVACION_PENDIENTE = "activacion_pendiente";
+const ESTADO_ACTIVO = "activo";
 
 module.exports = {
   ESTADO_ACTIVACION_PENDIENTE,
+  ESTADO_ACTIVO,
 };
